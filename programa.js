5e9d3dd9be8e1b1d16e8b50f9b037c09a606bf2a
JP Mejia <69003047+JPMejia2@users.noreply.github.com>
2021-01-18 13:37:55 -0600

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,9 @@
+var datoTexto = "Adios "
+var datoNumerico = 2.2
+var datoDecimal = 4.3645
+var datoChar = "a"
+var datosLogicos = true
+
+alert(datoTexto + " " + datoNumerico)
+
+alert(datoNumerico + datoDecimal)
